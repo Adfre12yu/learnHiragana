419d00e9ca69a8c3c18d3e0c5010ba5bccfc7ea1
Melih Yilmaz <melihyilmaz999@gmail,com>
2022-08-31 20:07:14 -0400

    Reworked Javascript

--- index.js
+++ index.js
@@ -1,94 +1,14 @@
-const question_el = document.getElementById("question-el");
-const btnOne = document.getElementById("answerOne");
-const btnTwo = document.getElementById("answerTwo");
-const btnThree = document.getElementById("answerThree");
-const btnFour = document.getElementById("answerFour");
-const body = document.getElementById("body");
-const correctAu = new Audio("audio/correct.wav");
-const wrongAu = new Audio("audio/wrong.wav");
+import questions from "./data.js";
+const questionEl = document.getElementById("question-el");
 const optionsBtn = document.getElementById("options-btn");
 const closeBtn = document.getElementById("close-btn");
-let canAnswer = true;
+const answers = document.getElementsByClassName("answer-btn");
+const correctAu = new Audio("audio/correct.wav");
+const wrongAu = new Audio("audio/wrong.wav");
 
-const buttons = {
-    1: btnOne,
-    2: btnTwo,
-    3: btnThree,
-    4: btnFour,
-};
-const questions = {
-    ん: "n",
-    わ: "wa",
-    を: "wo",
-    や: "ya",
-    ゆ: "yu",
-    よ: "yo",
-    ら: "ra",
-    り: "ri",
-    る: "ru",
-    れ: "re",
-    ろ: "ro",
-    ま: "ma",
-    み: "mi",
-    む: "mu",
-    め: "me",
-    も: "mo",
-    は: "ha",
-    ひ: "hi",
-    ふ: "fu",
-    へ: "he",
-    ほ: "ho",
-    な: "na",
-    に: "ni",
-    ぬ: "nu",
-    ね: "ne",
-    の: "no",
-    た: "ta",
-    ち: "chi",
-    つ: "tsu",
-    て: "te",
-    と: "to",
-    さ: "sa",
-    し: "shi",
-    す: "su",
-    せ: "se",
-    そ: "so",
-    か: "ka",
-    き: "ki",
-    く: "ku",
-    け: "ke",
-    こ: "ko",
-    あ: "a",
-    い: "i",
-    う: "u",
-    え: "e",
-    お: "o",
-    が: "ga",
-    ぎ: "gi",
-    ぐ: "gu",
-    げ: "ge",
-    ご: "go",
-    ざ: "za",
-    じ: "ji",
-    ず: "zu",
-    ぜ: "ze",
-    ぞ: "zo",
-    だ: "da",
-    ぢ: "ji",
-    づ: "zu",
-    で: "de",
-    ど: "do",
-    ば: "ba",
-    び: "bi",
-    ぶ: "bu",
-    べ: "be",
-    ぼ: "bo",
-    ぱ: "pa",
-    ぴ: "pi",
-    ぷ: "pu",
-    ぺ: "pe",
-    ぽ: "po",
-};
+let canAnswer = true;
+let rightBtn;
+
 const reversed = Object.fromEntries(Object.entries(questions).map(([key, value]) => [value, key]));
 
 const documentHeight = () => {
@@ -98,100 +18,41 @@ const documentHeight = () => {
 
 function newQuestion() {
     let question = Math.floor(Math.random() * 71);
-    let rightBtn = Math.floor(Math.random() * 4) + 1;
+    rightBtn = Math.floor(Math.random() * 4);
     let copy = JSON.parse(JSON.stringify(Object.values(questions)));
     copy.splice(question, 1);
-    for (let i = 1; i < 5; i++) {
+    for (let i = 0; i < 4; i++) {
         if (i == rightBtn) {
-            buttons[rightBtn].textContent = Object.values(questions)[question];
-            buttons[rightBtn].classList.add("correct");
+            answers[i].textContent = Object.values(questions)[question];
         } else {
             let wrongAnswer = Math.floor(Math.random() * copy.length);
-            buttons[i].textContent = copy[wrongAnswer];
+            answers[i].textContent = copy[wrongAnswer];
             copy.splice(wrongAnswer, 1);
         }
     }
-    question_el.textContent = reversed[buttons[rightBtn].textContent];
+    questionEl.textContent = reversed[answers[rightBtn].textContent];
 }
 
-function correctAnswer() {
+function answer(correct) {
     canAnswer = false;
-    for (let i = 1; i < 5; i++) {
-        if (buttons[i].classList.contains("correct")) {
-            buttons[i].classList.remove("correct");
-            buttons[i].classList.add("greenBtn");
-        }
-    }
 
-    correctAu.play();
+    correct ? correctAu.play() : wrongAu.play();
 
     setTimeout(function () {
-        body.classList.remove("greenBackground");
-        for (let i = 1; i < 5; i++) {
-            buttons[i].classList.remove("greenBtn");
-        }
         newQuestion();
         canAnswer = true;
     }, 500);
 }
 
-function wrongAnswer() {
-    canAnswer = false;
-    for (let i = 1; i < 5; i++) {
-        if (buttons[i].classList.contains("correct")) {
-            buttons[i].classList.remove("correct");
-            buttons[i].classList.add("greenBtn");
+for (let i = 0; i < 4; i++) {
+    let currentBtn = answers[i];
+    currentBtn.addEventListener("click", () => {
+        if (canAnswer) {
+            currentBtn.id == rightBtn ? answer(true) : answer(false);
         }
-    }
-
-    wrongAu.play();
-
-    setTimeout(function () {
-        body.classList.remove("greenBackground");
-        for (let i = 1; i < 5; i++) {
-            buttons[i].classList.remove("greenBtn");
-        }
-        newQuestion();
-        canAnswer = true;
-    }, 500);
+    });
 }
 
-btnOne.addEventListener("click", function () {
-    if (canAnswer) {
-        if (btnOne.classList.contains("correct")) {
-            correctAnswer();
-        } else {
-            wrongAnswer();
-        }
-    }
-});
-btnTwo.addEventListener("click", function () {
-    if (canAnswer) {
-        if (btnTwo.classList.contains("correct")) {
-            correctAnswer();
-        } else {
-            wrongAnswer();
-        }
-    }
-});
-btnThree.addEventListener("click", function () {
-    if (canAnswer) {
-        if (btnThree.classList.contains("correct")) {
-            correctAnswer();
-        } else {
-            wrongAnswer();
-        }
-    }
-});
-btnFour.addEventListener("click", function () {
-    if (canAnswer) {
-        if (btnFour.classList.contains("correct")) {
-            correctAnswer();
-        } else {
-            wrongAnswer();
-        }
-    }
-});
 optionsBtn.addEventListener("click", function () {
     overlay.style.display = "flex";
 });
